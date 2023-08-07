3b2c02c5f5657c07ca23bc3b52ca5c2888d2a69d
Vinayak Kannan <vinayak1000@gmail.com>
2023-08-06 21:00:13 -0400

    first commit

--- src/manager.ts
+++ src/manager.ts
@@ -22,7 +22,7 @@ addons.register(ADDON_ID, () => {
   // Register the panel
   addons.add(PANEL_ID, {
     type: types.PANEL,
-    title: "My addon",
+    title: "My Update",
     match: ({ viewMode }) => viewMode === "story",
     render: Panel,
   });
